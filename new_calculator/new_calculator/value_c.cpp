15426ad18eb1d914cf80af63abc5cc26b40ee44f
zhangxiaoxu <zhangxiaoxu@gbase.cn>
2021-07-15 15:36:10 +0800

    增加切词元素类

---
+++ new_calculator/new_calculator/value_c.cpp
@@ -0,0 +1,9 @@
+#include "value_c.h"
+value_c::value_c()
+{
+
+}
+value_c::~value_c()
+{
+
+}
---
+++ new_calculator/new_calculator/value_c.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+using namespace std;
+//每个value_c类的对象都对应一个对语句切词后的元素
+//此类贯穿执行模块的始终，每个模块的处理结果几乎都会在此类中写入信息
+//如元素的类型（数字，函数名，变量名，+ - * / 等）
+//next用于存储表达式下一个元素
+//vertical用于存储垂直方向的内容，如遇到函数，变量，则在垂直方向解决。
+class value_c
+{
+public:
+	value_c();
+	~value_c();
+private:
+	string m_data;
+	int m_validNum;
+	int m_pos;
+	value_c *next;
+	value_c *vertical;
+};
+
